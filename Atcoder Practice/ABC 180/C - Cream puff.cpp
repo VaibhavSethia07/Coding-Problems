4cc8f5a75a5e9d469bc8aca048b08608da00cb04
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2020-12-19 21:37:05 +0530

    Create C - Cream puff Solution

---
+++ Atcoder Practice/ABC 180/C - Cream puff.cpp
@@ -0,0 +1,57 @@
+
+// Problem: C - Cream puff
+// Contest: AtCoder - AtCoder Beginner Contest 180
+// URL: https://atcoder.jp/contests/abc180/tasks/abc180_c
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+
+#define FOR(i,a,b) for (int i = (a); i<(b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for(int i = (b)-1; i>=(a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+
+int main(){
+	long N;
+	cin>>N;
+	set<long> s;
+	for(long i=1;i*i<=N;i++){
+		if((N%i)==0){
+			s.insert(i);
+			s.insert(N/i);
+		}
+	}
+
+	for(auto& x:s)
+		cout<<x<<"\n";
+	return 0;	
+}
