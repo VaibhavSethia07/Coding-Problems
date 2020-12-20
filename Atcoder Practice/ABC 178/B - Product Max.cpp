d1025a593fdb1895b8f7898fea7ba2d795619fa0
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2020-12-20 20:22:55 +0530

    Create B - Product Max Solution

---
+++ Atcoder Practice/ABC 178/B - Product Max.cpp
@@ -0,0 +1,50 @@
+
+// Problem: B - Product Max
+// Contest: AtCoder - AtCoder Beginner Contest 178
+// URL: https://atcoder.jp/contests/abc178/tasks/abc178_b
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+
+#define FOR(i,a,b) for (int i = (a); i<(b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for(int i = (b)-1; i>=(a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+
+int main(){
+	long long a,b,c,d;
+	cin>>a>>b>>c>>d;
+
+	cout<<max(max(b*d,a*c),max(b*c,a*d))<<"\n";
+	
+	return 0;	
+}
