1a16d97b8f2be5c1b85e3737e34e2551923e54de
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-01-07 21:30:56 +0530

    Create B - Minor Change Solution

---
+++ Atcoder Practice/ABC 172/B - Minor Change.cpp
@@ -0,0 +1,87 @@
+
+// Problem: B - Minor Change
+// Contest: AtCoder - AtCoder Beginner Contest 172
+// URL: https://atcoder.jp/contests/abc172/tasks/abc172_b
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=1;
+		if(N%2==0){
+			while((N%2)==0){
+				N/=2;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=1;
+			while((N%i)==0){
+				N/=i;
+				cnt++;
+			}
+			d[i]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=2;
+}
+
+void input(vl& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+void solve(vl& v,int N){
+	
+}
+
+int main(){
+	string s1,s2;
+	cin>>s1>>s2;
+	long ans=0;
+	for(int i=0;i<(int)s1.length();i++)
+		if(s1[i]!=s2[i])
+			ans++;
+	cout<<ans<<"\n";
+	return 0;	
+}
