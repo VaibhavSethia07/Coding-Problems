fee027c7007a235ae49a23abf8e3df9bee20de53
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-01-20 18:49:44 +0530

    Create Birthday Party Solution

---
+++ USACO/Depth First Search/Birthday Party.cpp
@@ -0,0 +1,323 @@
+
+// Problem: Birthday Party
+// Contest: Kattis
+// URL: https://open.kattis.com/problems/birthday
+// Memory Limit: 1024 MB
+// Time Limit: 5000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=1;
+		if(N%2==0){
+			while((N%2)==0){
+				N/=2;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=1;
+			while((N%i)==0){
+				N/=i;
+				cnt++;
+			}
+			d[i]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=2;
+}
+
+int V,E,a,b;
+int g[105][105];
+vector<bool> vis;
+vector<pair<int,int>> edges;
+void input(vl& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+void dfs(int u){
+	vis[u]=true;
+	for(int v=0;v<V;v++){
+		if(!vis[v] && g[u][v]==1)
+			dfs(v);
+	}
+}
+
+bool solve(int V){
+	for(int i=0;i<V;i++){
+		for(int j=0;j<V;j++){
+			vis.clear();
+			vis.resize(V);
+			if(g[i][j]){
+				g[i][j]=g[j][i]=0;
+				dfs(0);
+				for(int k=0;k<V;k++){
+					if(!vis[k])
+						return true;
+				}
+				g[i][j]=g[j][i]=1;
+			}
+		}
+	}
+	return false;
+}
+
+int main(){
+	
+	while(true){
+		cin>>V>>E;
+		if(V==0 && E==0)
+			break;
+		memset(g,0,sizeof(g));
+		for(int i=0;i<E;i++){
+			cin>>a>>b;
+			g[a][b]=1;
+			g[b][a]=1;
+		}
+		
+		if(solve(V))
+			cout<<"Yes\n";
+		else
+			cout<<"No\n";
+		
+	}
+	
+	return 0;	
+}
+
+/* Editorial Soluton */
+
+// Problem: Birthday Party
+// Contest: Kattis
+// URL: https://open.kattis.com/problems/birthday
+// Memory Limit: 1024 MB
+// Time Limit: 5000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=1;
+		if(N%2==0){
+			while((N%2)==0){
+				N/=2;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=1;
+			while((N%i)==0){
+				N/=i;
+				cnt++;
+			}
+			d[i]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=2;
+}
+
+int V,E,a,b;
+int g[105][105];
+vector<bool> vis;
+vector<pair<int,int>> edges;
+void input(vl& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+void dfs(int u){
+	vis[u]=true;
+	for(int v=0;v<V;v++){
+		if(!vis[v] && g[u][v]==1)
+			dfs(v);
+	}
+}
+
+bool solve(int V){
+	for(auto& p:edges){
+		vis.clear();
+		vis.resize(V);
+		if(g[p.f][p.s]){
+			g[p.f][p.s]=g[p.s][p.f]=0;
+			dfs(0);
+			for(int k=0;k<V;k++){
+				if(!vis[k])
+					return true;
+			}
+			g[p.f][p.s]=g[p.s][p.f]=1;
+		}
+	}
+	return false;
+}
+
+int main(){
+	
+	while(true){
+		cin>>V>>E;
+		if(V==0 && E==0)
+			break;
+		memset(g,0,sizeof(g));
+		edges.clear();
+		edges.resize(E);
+		for(int i=0;i<E;i++){
+			cin>>a>>b;
+			g[a][b]=1;
+			g[b][a]=1;
+			edges[i]={a,b};
+		}
+		
+		if(solve(V))
+			cout<<"Yes\n";
+		else
+			cout<<"No\n";
+		
+	}
+	
+	return 0;	
+}
