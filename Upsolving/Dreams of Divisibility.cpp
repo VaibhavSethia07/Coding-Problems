271560be2f95eb7685463fa4ce0db6e59c4f8782
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-01-31 21:43:29 +0530

    Create Dreams of Divisibility Solution

---
+++ Upsolving/Dreams of Divisibility.cpp
@@ -0,0 +1,144 @@
+
+// Problem: Dreams of Divisibility
+// Contest: CodeChef - January Lunchtime 2021 Division 3
+// URL: https://www.codechef.com/LTIME92C/problems/DREDIV
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define modinv(a,m) modEx(a,m-2,m)
+#define moddiv(a,b,m) (a*modinv(b,m))%m
+const ll mod=1e9+7;
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=0;
+		if(N%div==0){
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=0;
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=1;
+}
+
+void input(vl& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+bool solve(vl& v,int N,int K){
+	
+	while((K%2)==0){
+		K/=2;
+	}
+	
+	for(int i=0;i<N;i++){
+		while((v[i]%2)==0){
+			v[i]/=2;
+		}
+		
+		if((v[i]%K)!=0)
+			return false;
+	}
+	
+	return true;
+}
+
+int main(){
+	
+	int T,N,K;
+	cin>>T;
+	while(T--){
+		cin>>N>>K;
+		vl v(N);
+		input(v,N);
+		if(solve(v,N,K))
+			cout<<"YES\n";
+		else
+			cout<<"NO\n";
+	}
+	
+	return 0;	
+}
