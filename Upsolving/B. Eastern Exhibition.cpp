2a7e4a9415f7ac02ed38be44b2f7bce36315fa54
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-02-20 16:26:31 +0530

    Codeforces Round #703 (Div. 2) Solution

---
+++ Upsolving/B. Eastern Exhibition.cpp
@@ -0,0 +1,138 @@
+// Problem: B. Eastern Exhibition
+// Contest: Codeforces - Codeforces Round #703 (Div. 2)
+// URL: https://codeforces.com/contest/1486/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define modinv(a,m) modEx(a,m-2,m)
+#define moddiv(a,b,m) (a*modinv(b,m))%m
+const ll mod=1e9+7;
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=0;
+		if(N%div==0){
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=0;
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=1;
+}
+
+void input(vector<long>& x,vector<long>& y,int N){
+	for(int i=0;i<N;i++){
+		cin>>x[i]>>y[i];
+	}
+}
+
+void solve(vector<long>& x,vector<long>& y,int N){
+	
+	sort(all(x));
+	sort(all(y));
+	
+	if(N&1)
+		cout<<1<<"\n";
+	else
+		cout<<(x[(N+2)/2-1]-x[(N+1)/2-1]+1)*1ll*(y[(N+2)/2-1]-y[(N+1)/2-1]+1)<<"\n";
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	
+	int T,N;
+	cin>>T;
+	while(T--){
+		cin>>N;
+		vector<long> x(N);
+		vector<long> y(N);
+		input(x,y,N);
+		solve(x,y,N);
+	}
+	
+	return 0;	
+}
