b86fbd906ab9735ad05ffd736140325af79a61d2
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-04-25 09:14:43 +0530

    Create Removing Digits Solution

---
+++ USACO/Dynamic Programming/Removing Digits.cpp
@@ -0,0 +1,159 @@
+// Problem: Removing Digits
+// Contest: CSES - CSES Problem Set
+// URL: https://cses.fi/problemset/task/1637
+// Memory Limit: 512 MB
+// Time Limit: 1000 ms
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define modinv(a,m) modEx(a,m-2,m)
+#define moddiv(a,b,m) (a*modinv(b,m))%m
+const ll mod=1e9+7;
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=0;
+		if(N%div==0){
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=0;
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=1;
+}
+
+/* Print the string of digits without leading zero */
+void norm(string& str){
+	bool zero=true;
+	
+	for(auto& c:str){
+		if(c!='0')
+			zero=false;
+		if(c!='0' || !zero)
+			cout<<c;
+	}
+}
+
+void input(vl& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+const int MAXN=1e6+1;
+int dp[MAXN];
+int solve(int n){
+	if(n<=9)
+		return 1;
+	if(dp[n]!=-1)
+		return dp[n];
+	int mn=1e9;
+	vector<int> digits;
+	int m=n;
+	while(m>0){
+		digits.push_back(m%10);
+		m/=10;
+	}
+	
+	for(int i=0;i<(int)digits.size();i++){
+		if(digits[i]==0)
+			continue;
+		mn=min(mn,1+solve(n-digits[i]));
+	}
+	
+	return dp[n]=mn;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	int n;
+	cin>>n;
+	memset(dp,-1,sizeof(dp));
+	dp[0]=0;
+	
+	// for(int i=1;i<=n;i++)
+		// dp[i]=solve(i);
+	// cout<<dp[n];
+	cout<<solve(n);
+	return 0;	
+}
