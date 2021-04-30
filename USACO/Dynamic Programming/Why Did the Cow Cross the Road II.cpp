95ca115de65780a7dd14596f8cf5fe3edc75699d
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-04-30 09:34:09 +0530

    Create Why Did Cow Cross Road II (Gold) LCS

---
+++ USACO/Dynamic Programming/Why Did the Cow Cross the Road II.cpp
@@ -0,0 +1,145 @@
+// Problem: Problem 2. Why Did the Cow Cross the Road II
+// Contest: USACO - USACO 2017 February Contest, Gold
+// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=718
+// Memory Limit: 256 MB
+// Time Limit: 4000 ms
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define modinv(a,m) modEx(a,m-2,m)
+#define moddiv(a,b,m) (a*modinv(b,m))%m
+const ll mod=1e9+7;
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=0;
+		if(N%div==0){
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=0;
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=1;
+}
+
+/* Print the string of digits without leading zero */
+void norm(string& str){
+	bool zero=true;
+	
+	for(auto& c:str){
+		if(c!='0')
+			zero=false;
+		if(c!='0' || !zero)
+			cout<<c;
+	}
+}
+
+const int MAXN=1e3+5;
+int a[MAXN];
+int b[MAXN];
+int n;
+int dp[MAXN][MAXN];
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	freopen("nocross.in","r",stdin);
+	freopen("nocross.out","w",stdout);
+	
+	cin>>n;
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	for(int i=0;i<n;i++)
+		cin>>b[i];
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			if(abs(a[i-1]-b[j-1])<=4)
+				dp[i][j]=dp[i-1][j-1]+1;
+			else
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		}
+	}
+	cout<<dp[n][n];
+	
+	return 0;	
+}
