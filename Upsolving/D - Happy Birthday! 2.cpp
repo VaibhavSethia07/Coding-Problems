bde7b71ea4d448f778fa4e685b62aa93820eecdd
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-05-09 19:38:11 +0530

    Upsolve KYOCERA ABC-200 (Pigeonhole Principle)

---
+++ Upsolving/D - Happy Birthday! 2.cpp
@@ -0,0 +1,167 @@
+// Problem: D - Happy Birthday! 2
+// Contest: AtCoder - KYOCERA Programming Contest 2021（AtCoder Beginner Contest 200）
+// URL: https://atcoder.jp/contests/abc200/tasks/abc200_d
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define modinv(a,m) modEx(a,m-2,m)
+#define moddiv(a,b,m) (a*modinv(b,m))%m
+const int mod=200;
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=0;
+		if(N%div==0){
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=0;
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=1;
+}
+
+/* Print the string of digits without leading zero */
+void norm(string& str){
+	bool zero=true;
+	
+	for(auto& c:str){
+		if(c!='0')
+			zero=false;
+		if(c!='0' || !zero)
+			cout<<c;
+	}
+}
+
+void input(vi& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+void solve(vi& v,int N){
+	
+	map<int,vector<int>> cache;
+	for(int i=1;i<(1<<min(10,N));i++){
+		vector<int> a;
+		int sum=0;
+		for(int j=0;j<min(10,N);j++){
+			if(i&(1<<j)){
+				sum+=v[j];
+				sum%=mod;
+				a.push_back(j+1);
+			}
+		}
+		if(cache.count(sum)){
+			cout<<"Yes\n"<<cache[sum].size()<<" ";
+			for(auto& x:cache[sum])
+				cout<<x<<" ";
+			cout<<"\n";
+			cout<<a.size()<<" ";
+			for(auto& x:a)
+				cout<<x<<" ";
+			cout<<"\n";
+			return;
+		}else
+			cache[sum]=a;
+	}
+	cout<<"No\n";
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    	cin.tie(NULL);
+    
+
+	int N;
+	cin>>N;
+	vi v(N);
+	input(v,N);
+	solve(v,N);
+	
+	return 0;	
+}
