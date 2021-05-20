039054f9d66887cf3ee8aec8e1b7c1f8bb65677a
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-05-20 15:18:06 +0530

    (Silver) Bipartite + Combinatorics Solution

---
+++ USACO/Graph/The Great Revegetation.cpp
@@ -0,0 +1,183 @@
+// Problem: Problem 3. The Great Revegetation
+// Contest: USACO - USACO 2019 February Contest, Silver
+// URL: http://www.usaco.org/index.php?page=viewproblem2&cpid=920
+// Memory Limit: 256 MB
+// Time Limit: 4000 ms
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define modinv(a,m) modEx(a,m-2,m)
+#define moddiv(a,b,m) (a*modinv(b,m))%m
+const ll mod=1e9+7;
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=0;
+		if(N%div==0){
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=0;
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=1;
+}
+
+/* Print the string of digits without leading zero */
+void norm(string& str){
+	bool zero=true;
+	
+	for(auto& c:str){
+		if(c!='0')
+			zero=false;
+		if(c!='0' || !zero)
+			cout<<c;
+	}
+}
+
+int V,E,a,b;
+char ch;
+const int MAXN=1e5+5;
+vector<pair<int,char>> adj[MAXN];
+int color[MAXN];
+void input(vl& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+bool bipartite(int u,int col=1){
+	color[u]=col;
+	
+	bool ok=1;
+	for(pair<int,char> p:adj[u]){
+		if(color[p.f]==0){
+			if(p.s=='D')
+				ok&=bipartite(p.f,3-color[u]);
+			else
+				ok&=bipartite(p.f,color[u]);
+		}else{
+			if(p.s=='S' && color[u]!=color[p.f])
+				return false;
+			if(p.s=='D' && color[u]==color[p.f])
+				return false;
+		}
+	}
+	return ok;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	freopen("revegetate.in","r",stdin);
+	freopen("revegetate.out","w",stdout);
+	
+	cin>>V>>E;
+	
+	for(int i=0;i<E;i++){
+		cin>>ch>>a>>b;
+		adj[a].push_back({b,ch});
+		adj[b].push_back({a,ch});
+	}
+	
+	int comp=0;
+	bool ok=1;
+	for(int u=1;u<=V;u++){
+		if(color[u]==0){
+			comp++;
+			ok&=bipartite(u);
+		}
+	}
+	
+	if(!ok){
+		cout<<0;
+		return 0;
+	}
+	
+	cout<<1;
+	while(comp--)
+		cout<<0;
+	return 0;	
+}
