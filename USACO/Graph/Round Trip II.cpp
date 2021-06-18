aada0f16706f95f5ad76081cf9d2ab33e0de6390
Vaibhav Sethia <E19CSE321@bennett.edu.in>
2021-06-18 11:35:25 +0530

    Cycle Detection and Restoring

---
+++ USACO/Graph/Round Trip II.cpp
@@ -0,0 +1,182 @@
+// Problem: Round Trip II
+// Contest: CSES - CSES Problem Set
+// URL: https://cses.fi/problemset/task/1678
+// Memory Limit: 512 MB
+// Time Limit: 1000 ms
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+using l = long;
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string;
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<ll>;
+
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define modinv(a,m) modEx(a,m-2,m)
+#define moddiv(a,b,m) (a*modinv(b,m))%m
+const ll mod=1e9+7;
+
+/* These template codes have been taken from cp-algorithms */
+ll extEuclid(ll a,ll b,ll& x,ll& y){
+	if(b==0){
+		x=1;
+		y=0;
+		return a;
+	}
+	
+	ll x1,y1;
+	ll d=extEuclid(b,a%b,x1,y1);
+	x=y1;
+	y=x1-(a/b)*y1;
+	return d;
+}
+
+ll modInverse(ll a,ll n){
+	ll x,y;
+	extEuclid(a,n,x,y);
+	return x;
+}
+
+ll linearCong(ll a,ll b,ll n){
+	ll d=__gcd(a,n);
+	
+	if(b%d)
+		return -1;
+	a/=d;b/=d;n/=d;
+	ll a_inv=modInverse(a,n);
+	ll ans=(b*a_inv)%n;
+	if(ans<0)
+		ans+=n;
+	return ans;
+}
+
+void divisors(map<int,int>& d,ll N){
+	for(int div : {2,3,5}){
+		int cnt=0;
+		if(N%div==0){
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+	}
+	static array<int,8> increments = {4,2,4,2,4,6,2,6};
+	int i=0;
+	for(ll div=7;div*div<=N;div+=increments[i++]){
+		if((N%div)==0){
+			int cnt=0;
+			while((N%div)==0){
+				N/=div;
+				cnt++;
+			}
+			d[div]=cnt;
+		}
+		if(i==8)
+			i=0;
+	}
+	if(N>1)
+		d[N]=1;
+}
+
+/* Print the string of digits without leading zero */
+void norm(string& str){
+	bool zero=true;
+	
+	for(auto& c:str){
+		if(c!='0')
+			zero=false;
+		if(c!='0' || !zero)
+			cout<<c;
+	}
+}
+
+const int MAXN=1e5+5;
+int V,E,a,b;
+vector<int> adj[MAXN];
+bool recSt[MAXN];
+bool vis[MAXN];
+vector<int> res;
+int start=0;
+void input(vl& v,int N){
+	for(int i=0;i<N;i++){
+		cin>>v[i];
+	}
+}
+
+bool dfs(int u){
+	vis[u]=1;
+	recSt[u]=1;
+	
+	for(auto& v:adj[u]){
+		if(!vis[v]){
+			if(dfs(v)){
+				if(start!=-1)
+					res.push_back(u);
+				if(start==u)
+					start=-1;
+				return true;
+			}
+		}else if(recSt[v]){
+			start=v;
+			res.push_back(v);
+			res.push_back(u);
+			return true;
+		}
+	}
+	recSt[u]=0;
+	return false;
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	cin>>V>>E;
+	
+	for(int i=1;i<=E;i++){
+		cin>>a>>b;
+		adj[a].push_back(b);
+	}
+	
+	for(int i=1;i<=V;i++){
+		if(!vis[i] && res.size()==0){
+			dfs(i);
+		}
+	}
+	
+	if((int)res.size()==0)
+		cout<<"IMPOSSIBLE\n";
+	else{
+		cout<<(int)res.size()<<"\n";
+		for(int i=(int)res.size()-1;i>=0;i--)
+			cout<<res[i]<<" ";
+	}
+	return 0;	
+}
